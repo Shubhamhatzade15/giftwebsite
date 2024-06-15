b85740633840167313cbc9eb34f886b8a82519e6
Shubham Hatzade <71142023+Shubhamhatzade15@users.noreply.github.com>
2024-06-15 06:43:48 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import Footer from "./components/Footer";
 
 
 export const metadata: Metadata = {
-  title: "Sample app",
-  description: "My First app",
+  title: "Nextjs App",
+  description: "Shubham Hatzade",
 };
 
 export default function RootLayout({
